263e6872428913b30b3821edc05e622c42445735
Hyein <976hannah@snu.ac.kr>
2026-04-28 17:41:56 +0900

    the test data is not a real test

--- github/workflows/Hyein/meta_executor.py
+++ github/workflows/Hyein/meta_executor.py
@@ -6,7 +6,7 @@ def main():
         # "github/workflows/Hyein/material_NN_tuning.py",
         # "github/workflows/Hyein/material_NN_SHAP.py",
         "github/workflows/Hyein/material_KAN_sweep.py",
-        "github/workflows/Hyein/material_KAN_analyze.py",
+        # "github/workflows/Hyein/material_KAN_analyze.py",
         # "github/workflows/Hyein/material_NN_for_KANrange.py",
     ]
     target_data = [
@@ -14,8 +14,8 @@ def main():
         # 'CO2RRLCA',
         # 'CO2RRNPV',
         # 'CO2RRMSP',
-        'CO2HPx10v2',
-        # 'CO2HEx10v2', # v2: 3 variables are excluded
+        'CO2HPx10',
+        'CO2HEx10', # v2: 3 variables are excluded / v3: x9 & x16 excluded
         # 'CO2RRCC', 'CO2RRRA', 'CO2RRC', 'CO2RRA', 'CO2RREE', 'CO2RRCA',
         # 'CO2RRE', 'CO2RRRC', 'CO2RRUR',
         # 'AutoAM', 'Perovskite',
@@ -36,6 +36,7 @@ def main():
     #     # "github/workflows/Hyein/toy_NN_SHAP_Sobol.py",
     # ]
     # target_data = [
+    #     'ishigami', 'rosenbrock',     #TODO: Try with these classical examples
     #     # 'convolution', 'original', 'mult_periodic',
     #     'exponential', 'logarithm',
     #     # 'multiplication', 'conditional'
