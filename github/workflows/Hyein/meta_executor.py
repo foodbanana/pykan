f6e2a20aa169836e7a3745109b97d68f5232e23c
Hyein <976hannah@snu.ac.kr>
2026-05-08 17:54:16 +0900

    2nd order Sobol' indices

--- github/workflows/Hyein/meta_executor.py
+++ github/workflows/Hyein/meta_executor.py
@@ -10,14 +10,14 @@ def main():
         # "github/workflows/Hyein/material_NN_for_KANrange.py",
     ]
     target_data = [
-        # 'AgNP',
-        # 'CO2RRLCA',
-        # 'CO2RRNPV',
-        # 'CO2RRMSP',
-        'CO2HPx10',
-        'CO2HEx10', # v2: 3 variables are excluded / v3: x9 & x16 excluded
-        'ITH4500',
-        'ITHH4500',
+        'AgNP',
+        'CO2RRLCA',
+        'CO2RRNPV',
+        'CO2RRMSP',
+        # 'CO2HPx10',
+        # 'CO2HEx10', # v2: 3 variables are excluded / v3: x9 & x16 excluded
+        # 'ITH4500',
+        # 'ITHH4500',
         # 'CO2RRCC', 'CO2RRRA', 'CO2RRC', 'CO2RRA', 'CO2RREE', 'CO2RRCA',
         # 'CO2RRE', 'CO2RRRC', 'CO2RRUR',
         # 'AutoAM', 'Perovskite',
